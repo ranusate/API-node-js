9994597b898ac5bdab9499cc6a49953ca85df36c
radianus01 <ranusate151126@gmail.com>
2020-06-30 11:43:36 +0700

    3 commit

--- controller.js
+++ controller.js
@@ -16,3 +16,72 @@ exports.users = function (req, res) {
 exports.index = function (req, res) {
   response.ok("Hello from the Node JS RESTful side!", res);
 };
+
+exports.findUsers = function (req, res) {
+  var cust_id = req.params.cust_id;
+
+  connection.query("SELECT * FROM customer where id = ?", [cust_id], function (
+    error,
+    rows,
+    fields
+  ) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+exports.createUsers = function (req, res) {
+  var nama = req.body.first_name;
+  var jk = req.body.jk;
+  var no_tlpn = req.body.no_tlpn;
+  var alamat = req.body.alamat;
+
+  connection.query(
+    "INSERT INTO customer (nama, jk,no_tlpn, alamat) values (?,?,?,?)",
+    [nama, jk, no_tlpn, alamat],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil menambahkan customer!", res);
+      }
+    }
+  );
+};
+
+exports.updateUsers = function (req, res) {
+  var cust_id = req.body.cust_id;
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+
+  connection.query(
+    "UPDATE customer SET first_name = ?, last_name = ? WHERE id = ?",
+    [first_name, last_name, cust_id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil merubah user!", res);
+      }
+    }
+  );
+};
+
+exports.deleteUsers = function (req, res) {
+  var cust_id = req.body.cust_id;
+
+  connection.query("DELETE FROM customer WHERE id = ?", [cust_id], function (
+    error,
+    rows,
+    fields
+  ) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok("Berhasil menghapus user!", res);
+    }
+  });
+};
--- routes.js
+++ routes.js
@@ -6,4 +6,12 @@ module.exports = function (app) {
   app.route("/").get(todoList.index);
 
   app.route("/users").get(todoList.users);
+
+  app.route("/users/:user_id").get(todoList.findUsers);
+
+  app.route("/users").post(todoList.createUsers);
+
+  app.route("/users").put(todoList.updateUsers);
+
+  app.route("/users").delete(todoList.deleteUsers);
 };
